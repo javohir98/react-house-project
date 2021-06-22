2fd9109e0c0fce4b9b6c3cab5e5e53e9feb98758
javohir98 <47714181+javohir98@users.noreply.github.com>
2021-06-23 01:17:45 +0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import './App.scss'
-import './responsive.scss'
 import ChoicePlace from './components/ChoicePlace'
 import Footer from './components/footer/Footer'
 import GoodHandsLeft from './components/GoodHandsLeft'
